a0c021c63ff3f0b6cc663a3602add22a4d7eacf0
irina <irinafrutiz@gmail.com>
2023-04-26 14:15:30 +0200

    block 4.2.6: add an exeption NoSuchElementException. fix the test with error.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -5,3 +7,15 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
