764864087e4476cc6279037cdfb269c9296702de
irina <irinafrutiz@gmail.com>
2023-04-26 13:30:24 +0200

    task 4.2.3. changed main_page.py. created class MainPage.

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
