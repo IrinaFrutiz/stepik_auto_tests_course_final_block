8003b05d4e6fabd9e10c56717583b582f067de6c
irina <irinafrutiz@gmail.com>
2023-04-29 20:26:08 +0200

    add test for exercise 4_3_4, add xfail mark to one of the links.

--- test_product_page.py
+++ test_product_page.py
@@ -1,36 +1,55 @@
-import math
-from selenium.common import NoAlertPresentException
+import time
+from telnetlib import EC
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from pages.locators import ProductPageLocators
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser, language):
-    link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/?promo=newYear2019'
+links = [
+    "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+]
+
+@pytest.mark.parametrize("product", links)
+def test_guest_can_add_product_to_basket(browser, product: str) -> None:
+    link = product
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_page()
-    # batton = browser.find_element(*ProductPageLocators.PRODUCT_ADD_TO_BASKET)
-    # batton.click()
-    # # alert = BasePage(browser)
-    # # alert.solve_quiz_and_get_code()
-    # alert = browser.switch_to.alert
-    # x = alert.text.split(" ")[2]
-    # answer = str(math.log(abs((12 * math.sin(int(x))))))
-    # alert.send_keys(answer)
-    # alert.accept()
-    # try:
-    #     alert = browser.switch_to.alert
-    #     alert_text = alert.text
-    #     print(f"Your code: {alert_text}")
-    #     alert.accept()
-    # except NoAlertPresentException:
-    #     print("No second alert presented")
+    page.should_click_to_button_add_to_basket()
+    page.solve_quiz_and_get_code
 
 
-def test_guest_check_data(browser, language)
-    link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize("product", links)
+def test_guest_check_data(browser, product: str) -> None:
+    link = product
     page = ProductPage(browser, link)
     page.open()
     page.should_check_book_name()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+                                  ])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_click_to_button_add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_message_book_added_to_basket()
+    page.should_be_equel_sum_on_basket_and_book
+    # time.sleep(50)
+
 
 
