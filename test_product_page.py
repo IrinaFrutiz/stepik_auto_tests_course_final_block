90a33292503964d9b98854dd28972cab28a1137b
irina <irinafrutiz@gmail.com>
2023-05-05 12:28:48 +0200

    4.3.14 : add tag need_review

--- test_product_page.py
+++ test_product_page.py
@@ -23,7 +23,7 @@ def test_guest_can_add_product_to_basket_new_year(browser, link):
     page.check_message_book_added_to_basket()
     page.should_be_equal_sum_on_basket_and_book
 
-
+@pytest.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -73,12 +73,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
@@ -106,6 +108,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_no_massage_after_adding_product_to_basket()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket_new_year(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
         page = ProductPage(browser, link)
