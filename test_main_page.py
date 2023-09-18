ee12e971b278c4e0df489cecba3f12adc9b4f2de
irina <irinafrutiz@gmail.com>
2023-09-18 20:18:37 +0200

    change code: add allure

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,16 @@
+import allure
 import pytest
-
 from pages.locators import BasePageLocators
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.basket_page import BasketPage
+# import conftest
+
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage():
+@allure.feature("LoginFromMainPage")
+class TestLoginFromMainPage:
+    @allure.title("Check go to login page")
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
@@ -15,18 +19,23 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    def test_guest_should_see_login_link(self,browser):
+    @allure.title("Check see login link")
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
+
+@allure.title("Check guest_should_be_login_page")
 def test_guest_should_be_login_page(browser):
     link = f'https://selenium1py.pythonanywhere.com/accounts/login/'
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
 
+
+@allure.title("Check guest_cant_see_product_in_basket_opened_from_main_page")
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
     page = LoginPage(browser, link)
--- test_product_page.py
+++ test_product_page.py
@@ -1,23 +1,29 @@
+import allure
 import pytest
 from pages.base_page import BasePage
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 import time
+import conftest
 
 
-@pytest.mark.parametrize("link", ["https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
-                                "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-                                    ])
+@allure.title("Check guest_can_add_product_to_basket_new_year")
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"])
 def test_guest_can_add_product_to_basket_new_year(browser, link):
     page = ProductPage(browser, link)
     page.open()
+    time.sleep(10)
     page.should_be_product_page()
     page.should_click_to_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.check_message_book_added_to_basket()
     page.should_be_equal_sum_on_basket_and_book()
 
+
+@allure.title("Check guest_can_add_product_to_basket")
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -26,7 +32,9 @@ def test_guest_can_add_product_to_basket_new_year(browser, link):
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  pytest.param(
+                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                    marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
                                   ])
@@ -38,6 +46,8 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.check_message_book_added_to_basket()
     page.should_be_equal_sum_on_basket_and_book()
 
+
+@allure.title("Check guest_cant_see_success_message_after_adding_product_to_basket")
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -47,12 +57,16 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_no_massage_after_adding_product_to_basket()
 
+
+@allure.title("Check guest_cant_see_success_message")
 def test_guest_cant_see_success_message(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_no_massage_after_adding_product_to_basket()
 
+
+@allure.title("Check message_disappeared_after_adding_product_to_basket")
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -62,12 +76,16 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_desappeared_massage_after_adding_product_to_basket()
 
+
+@allure.title("Check guest_should_see_login_link_on_product_page")
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+
+@allure.title("Check guest_can_go_to_login_page_from_product_page")
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -75,6 +93,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+
+@allure.title("Check guest_cant_see_product_in_basket_opened_from_product_page")
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -85,8 +105,10 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket.check_basket_is_empty()
     basket.check_text_that_basket_is_empty()
 
+
+@allure.feature("Check UserAddToBasketFromProductPage")
 @pytest.mark.login
-class TestUserAddToBasketFromProductPage():
+class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/ru/accounts/login/'
@@ -97,12 +119,14 @@ class TestUserAddToBasketFromProductPage():
         self.login = BasePage(browser, link)
         self.login.should_be_authorized_user()
 
+    @allure.title("Check user_cant_see_success_message")
     def test_user_cant_see_success_message(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
         page = ProductPage(browser, link)
         page.open()
         page.should_no_massage_after_adding_product_to_basket()
 
+    @allure.title("Check user_can_add_product_to_basket")
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -113,5 +137,3 @@ class TestUserAddToBasketFromProductPage():
         page.solve_quiz_and_get_code()
         page.check_message_book_added_to_basket()
         page.should_be_equal_sum_on_basket_and_book()
-
-
