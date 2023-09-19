40c20b8646e3a6a9c1d4ec53a9dd1510a0953572
irina <irinafrutiz@gmail.com>
2023-09-19 11:40:54 +0200

    change file: fixing pep8 errors

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
+# Chrome
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+# FireFox
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FFService
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 
 def pytest_addoption(parser):
@@ -14,61 +16,32 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en-gb',
                      help="Choose language: '--language=en-gb' or '--language=ru'")
 
+
 @pytest.fixture(scope="function")
 def browser(request):
-        browser_name = request.config.getoption("browser_name")
-        user_language = request.config.getoption("language", default="en-gb")
-        chrome_options = Options()
-        # chrome_options.add_argument("--prefs=intl.accept_languages=en-gb")
-        chrome_options.add_experimental_option(
-            'prefs', {'intl.accept_languages': user_language})
-        # chrome_options.add_argument(f"--language={user_language}")
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language", default="en-gb")
+    chrome_options = Options()
+    chrome_options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
 
-        options_firefox = OptionsFirefox()
-        options_firefox.set_preference("intl.accept_languages", user_language)
-        browser = None
-        if browser_name == "chrome":
-            print("\nstart chrome browser for test..")
-            browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", user_language)
 
-        elif browser_name == "firefox":
-            print("\nstart firefox browser for test..")
-            browser = webdriver.Firefox(service=FFService(GeckoDriverManager().install()), options=options_firefox)
-        else:
-            raise pytest.UsageError("--browser_name should be chrome or firefox")
-        # if you need open browser on all window
-        # browser.maximize_window()
-        yield browser
-        print(f"\nquit {browser_name}..")
-        browser.quit()
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=chrome_options)
 
-# supported_languages = {
-#     'ar',
-#     'ca',
-#     'cs',
-#     'da',
-#     'de',
-#     'en-gb',
-#     'el',
-#     'es',
-#     'fi',
-#     'fr',
-#     'it',
-#     'ko',
-#     'nl',
-#     'pl',
-#     'pt',
-#     'pt-br',
-#     'ro',
-#     'ru',
-#     'sk',
-#     'uk',
-#     'zh-hans'
-# }
-# @pytest.fixture(scope="function")
-# def language(request):
-#     language = request.config.getoption("language")
-#     if language in supported_languages:
-#         return language
-#     else:
-#         raise pytest.UsageError("Choose the right language!")
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(
+            service=FFService(GeckoDriverManager().install()),
+            options=options_firefox)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.maximize_window()
+    yield browser
+    print(f"\nquit {browser_name}..")
+    browser.quit()
