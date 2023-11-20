4be095394972e5d8a6efd2e7e2f174b8ea0a6be5
irina <irinafrutiz@gmail.com>
2023-11-20 21:51:33 +0100

    change: locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,33 +2,31 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    VIEW_BASKET = (By.CSS_SELECTOR,
-                   "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_LINK_INVALID = (By.ID, "login_link_inc")
+    VIEW_BASKET = (By.XPATH, '//a[contains(text(), "View basket")]')
     TOTAL = (By.CSS_SELECTOR, "#basket_formset > div")
-    EMPTY_LINK = (By.CSS_SELECTOR, '#content_inner')
+    EMPTY_LINK = (By.ID, 'content_inner')
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class LoginPageLocators:
-    LOG_IN_FORM = (By.CSS_SELECTOR, "#login_form")
-    LOG_IN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
-    LOG_IN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOG_IN_FORM = (By.ID, "login_form")
+    LOG_IN_EMAIL = (By.ID, "id_login-username")
+    LOG_IN_PASSWORD = (By.ID, "id_login-password")
     LOG_IN_BUTTON = (By.NAME, "login_submit")
 
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
-    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
-    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
+    REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.ID, "id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, 'registration_submit')
 
 
 class ProductPageLocators:
-    PRODUCT_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
-    PRODUCT_BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
-    PRODUCT_BASKET_PRICE = (By.CSS_SELECTOR,
-                            '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
-    PRODUCT_NAME_FROM_BOOK = (By.CSS_SELECTOR, '#default > div.container-fluid.page > div > ul > li.active')
-    PRODUCT_NAME = (By.TAG_NAME, "h1")
-    PRODUCT_ADDED_TO_BASKET = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div')
+    PRODUCT_ADD_TO_BASKET = (By.XPATH, '//button[@value="Add to basket"]')
+    PRODUCT_BOOK_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    PRODUCT_BASKET_PRICE = (By.CSS_SELECTOR, 'div.alertinner > p > strong')
+    PRODUCT_NAME_FROM_BOOK = (By.CSS_SELECTOR, 'ul.breadcrumb > li.active')
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_ADDED_TO_BASKET = (By.XPATH, '(//div[@class="alertinner "])[1]')
