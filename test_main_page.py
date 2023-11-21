e01fac0ee4aa5158c6fffbfabd3693180b3e683b
irina <irinafrutiz@gmail.com>
2023-11-21 13:57:43 +0100

    add: general link, test_user_can_logout

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 import allure
 import pytest
+from faker import Faker
+
+from pages.base_page import BasePage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.basket_page import BasketPage
+
+
+link = 'http://selenium1py.pythonanywhere.com'
 
 
 @pytest.mark.login_guest
@@ -11,7 +17,6 @@ class TestLoginFromMainPage:
     @allure.title("Check go to login page by guest")
     @allure.severity(severity_level="CRITICAL")
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
@@ -20,7 +25,6 @@ class TestLoginFromMainPage:
 
     @allure.title("Check see login link")
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
@@ -28,18 +32,34 @@ class TestLoginFromMainPage:
 
 @allure.title("Check guest_should_be_login_page")
 def test_guest_should_be_login_page(browser):
-    link = f'https://selenium1py.pythonanywhere.com/accounts/login/'
     page = LoginPage(browser, link)
-    page.open()
     page.should_be_login_page()
 
 
 @allure.title("Check guest_cant_see_product_in_basket_opened_from_main_page")
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
     page = LoginPage(browser, link)
     page.open()
     page.go_to_view_basket()
     basket = BasketPage(browser, link)
     basket.check_basket_is_empty()
     basket.check_text_that_basket_is_empty()
+
+
+@allure.feature("Check User")
+@pytest.mark.login
+class TestsByUser:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        fake = Faker()
+        email = fake.email()
+        self.login = LoginPage(browser, link)
+        self.login.open()
+        self.login.register_new_user(email)
+        self.login = BasePage(browser, link)
+        self.login.should_be_authorized_user()
+
+    def test_user_can_logout(self, browser):
+        page = LoginPage(browser, link)
+        page.user_can_logout()
+        page.should_be_login_page()
