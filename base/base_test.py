1f4273278609041d5121be8c200bd0bc40c90052
irina <irinafrutiz@gmail.com>
2023-12-19 17:37:53 +0100

    change: add Data, delete Main

--- base/base_test.py
+++ base/base_test.py
@@ -4,21 +4,21 @@ from pages.login_page import LoginPage
 from base.base_page import BasePage
 from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
-from pages.main_page import MainPage
+from config.data import Data
+
 
 class BaseTest:
+    data: Data
     login_page: LoginPage
     base_page: BasePage
     basket_page: BasketPage
     product_page: ProductPage
-    main_page: MainPage
-
 
     @pytest.fixture(autouse=True)
     def setup(self, request, browser):
         request.cls.browser = browser
+        request.cls.data = Data()
         request.cls.login_page = LoginPage(browser)
         request.cls.base_page = BasePage(browser)
         request.cls.basket_page = BasketPage(browser)
         request.cls.product_page = ProductPage(browser)
-        request.cls.main_page = MainPage(browser)
